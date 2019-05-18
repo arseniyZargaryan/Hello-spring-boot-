c36efadddd5e894c9cd3e39bdbaee26421eed6de
ArseniyZargaryan <arsyush@mail.ru>
2019-05-19 03:19:13 +0800

    Hello world! ☺

---
+++ src/main/java/ru/mail/arseniyzargaryan/demo/ServerStartApplication.java
@@ -0,0 +1,13 @@
+package ru.mail.arseniyzargaryan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerStartApplication.class, args);
+    }
+
+}
